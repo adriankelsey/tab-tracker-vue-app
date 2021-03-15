e944113350ed44142a6999a8dda76144c185e401
Adrian <adrian@Adrians-MacBook-Pro.local>
2021-03-15 20:47:52 +0000

    created api & authentication services for post requests, created ui for registration page and successfullyy made post request

---
+++ client/src/components/Register.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="register">
+    <h1>register</h1>
+    <input
+      name="email"
+      type="email"
+      placeholder="enter email"
+      v-model="email"
+    />
+    <input
+      name="password"
+      type="password"
+      placeholder="enter password"
+      v-model="password"
+    />
+    <button @click="submit">submit</button>
+  </div>
+</template>
+
+<script>
+import Authentication from '@/services/Authentication'
+
+export default {
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    async submit() {
+      try {
+        const response = await Authentication.register({
+          email: this.email,
+          password: this.password
+        })
+        console.log(response.data)
+
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
